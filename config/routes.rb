2f0fb4d5ca500e9951e035d968d2ca0c660997ea
Ricardo Dullius <ricardodullius@gmail.com>
2014-03-20 15:02:16 -0300

    Changed the route to layout#application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Omrails::Application.routes.draw do
-root :to => "home#index"
+root :to => "layouts#application"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
