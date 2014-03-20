e711e2fb1545b1c75161b25fad23b8c4aaa90622
Ricardo Dullius <ricardodullius@gmail.com>
2014-03-20 14:25:37 -0300

    New route for root home only

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Omrails::Application.routes.draw do
+ root to: "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
